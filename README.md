84a4bf98fa2e345821063707e6c5dacec8882472
sachin chakrdhar <sachinchakradhar8@gmail.com>
2022-09-13 07:47:12 +0545

    check

---
+++ README.md
@@ -0,0 +1,9 @@
+# GridLayoutExample
+This is Jetpack Compose Grid Layout Example
+
+![gridView](https://user-images.githubusercontent.com/61373662/141764927-162e3d59-bbee-4877-8241-aedc43a691d5.gif)
+
+
+
+Watch this tutorial ON Youtube Click her👉👉
+https://youtu.be/GcY6tkewjh8
---
+++ app/src/main/java/com/malkinfo/gridlayoutexample/view/FruitsGridLayout.kt
@@ -0,0 +1,157 @@
+package com.malkinfo.gridlayoutexample.view
+
+import android.content.Context
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.malkinfo.gridlayoutexample.R
+import com.malkinfo.gridlayoutexample.model.FruitsData
+import com.malkinfo.gridlayoutexample.ui.theme.Purple500
+
+@ExperimentalFoundationApi
+@Composable
+fun FruitGrid(navController : NavController){
+
+    val context = LocalContext.current
+    val dataFileString = getJsonDataFromAsset(context,"FruitsList.json")
+    val gson = Gson()
+    val gridSampleType = object :TypeToken<List<FruitsData>>(){}.type
+    val fruitData : List<FruitsData> = gson.fromJson(dataFileString,gridSampleType)
+
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .background(color = Purple500)
+            .padding(6.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text ="Fruits Calories and Sugar",
+                color = Color.White,
+                fontSize =  20.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        LazyVerticalGrid(
+            cells = GridCells.Fixed(3),
+          modifier = Modifier.padding(10.dp)
+        ){
+            items(fruitData){data->
+                FruitDataGridItem(data,navController)
+
+            }
+
+        }
+
+    }
+
+
+
+}
+
+@Composable
+fun FruitDataGridItem(data:FruitsData,navController :NavController){
+
+    Card(modifier = Modifier
+        .clickable {
+            val itemVal = Gson().toJson(data)
+            navController.navigate("grid_detail/$itemVal")
+        }
+        .padding(10.dp)
+        .fillMaxSize(),
+        elevation =  5.dp,
+        shape = RoundedCornerShape(5.dp)
+    ) {
+        Column(modifier = Modifier) {
+            Image(painter = painterResource(
+                id =  when(data.id){
+                    1L-> R.drawable.lunges
+                    2L -> R.drawable.pushups
+                    3L -> R.drawable.squats
+                    4L -> R.drawable.burpees
+                    5L -> R.drawable.sideplank
+                    6L -> R.drawable.plank
+                    7L -> R.drawable.glute
+                    8L -> R.drawable.abnominal
+                    9L -> R.drawable.bent
+                    10L -> R.drawable.bridge
+                    11L -> R.drawable.straight
+                    12L -> R.drawable.bicycle
+                    13L -> R.drawable.stretch
+                    14L -> R.drawable.jumping
+                    15L -> R.drawable.mountain
+                    16L -> R.drawable.bear
+                    17L -> R.drawable.flutter
+                    else -> R.drawable.flutter
+                }
+            ),
+                contentDescription = "Grid Image",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(Alignment.CenterHorizontally)
+                    .clip(RoundedCornerShape(5.dp)),
+                alignment = Alignment.Center
+            )
+            Spacer(modifier = Modifier.padding(3.dp))
+            Text(
+                text = data.name,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally),
+                fontSize =  15.sp,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(modifier = Modifier.padding(1.dp))
+            Text(
+                text = data.desc,
+                modifier = Modifier
+                    .padding(7.dp,0.dp,0.dp,20.dp)
+                ,
+                fontSize =  13.sp,
+                fontWeight = FontWeight.Normal,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+
+            )
+
+
+        }
+
+    }
+
+
+
+}
+
+fun getJsonDataFromAsset(context: Context, data: String):String {
+    return context.assets.open(data).bufferedReader().use { it.readText() }
+
+}
